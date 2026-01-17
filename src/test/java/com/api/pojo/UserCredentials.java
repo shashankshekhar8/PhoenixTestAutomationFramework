86b28616e35f990fc04626bb8ad53ce69b9e5e16
shashank <10shashank10@gmail.com>
2026-01-17 07:13:32 +0530

    Updated the framework to Java 16
    Use the Record feature of Java 16 and converted all the pojo classes to Records in com.api.pojo package

--- src/test/java/com/api/pojo/UserCredentials.java
+++ src/test/java/com/api/pojo/UserCredentials.java
@@ -1,31 +1,5 @@
 package com.api.pojo;
 
-public class UserCredentials {
-	
-	String username;
-	String password;
-	
-	public UserCredentials(String username, String password) {
-		super();
-		this.username = username;
-		this.password = password;
-	}
+public record UserCredentials(String username, String password) {
 
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	
 }
--- src/test/java/com/api/tests/CreateJobAPITest.java
+++ src/test/java/com/api/tests/CreateJobAPITest.java
@@ -1,5 +1,12 @@
 package com.api.tests;
 
+import static io.restassured.RestAssured.given;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
 import org.testng.annotations.Test;
 
 import com.api.constants.Role;
@@ -8,15 +15,9 @@ import com.api.pojo.Customer;
 import com.api.pojo.CustomerAddress;
 import com.api.pojo.CustomerProduct;
 import com.api.pojo.Problems;
-import com.api.utils.AuthTokenProvider;
-import com.api.utils.ConfigManager;
 import com.api.utils.SpecUtil;
 
-import io.restassured.http.ContentType;
-
-import static io.restassured.RestAssured.*;
-
-import java.io.IOException;
+import io.restassured.module.jsv.JsonSchemaValidator;
 
 public class CreateJobAPITest {
 
@@ -25,18 +26,23 @@ public class CreateJobAPITest {
 		
 		Customer customer = new Customer("Shashank", "Shekhar", "9742666189", "", "101shashank@gmail.com", "");
 		CustomerAddress customerAddress = new CustomerAddress("106", "Shroff Soleno", "Baner Mahalunge road", "Near Orchid Hotel", "Mahalunge", "411045", "India", "Maharashtra");
-		CustomerProduct customerProduct = new CustomerProduct("2025-04-06T18:30:00.000Z", "35456430226557", "35456430226557", "35456430226557", "2025-04-06T18:30:00.000Z", 1 , 1);
+		CustomerProduct customerProduct = new CustomerProduct("2025-04-06T18:30:00.000Z", "59999999999999", "59999999999999", "59999999999999", "2025-04-06T18:30:00.000Z", 1 , 1);
 		Problems problems = new Problems(1, "Battery Issue");
-		Problems[] problemsArray = new Problems[1];
-		problemsArray[0] = problems;
-		CreateJobPayload createJobPayload = new CreateJobPayload(0, 2, 1, 1, customer, customerAddress, customerProduct, problemsArray);
+		
+		List<Problems> problemsList = new ArrayList<Problems>();
+		problemsList.add(problems); 
+		CreateJobPayload createJobPayload = new CreateJobPayload(0, 2, 1, 1, customer, customerAddress, customerProduct, problemsList); 
 		
 		given()
 			.spec(SpecUtil.requestSpecWithAuth(Role.FD, createJobPayload))
 		.when()
 			.post("/job/create")
 		.then()
-			.spec(SpecUtil.responseSpec_OK());
+			.spec(SpecUtil.responseSpec_OK())
+			.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("response-schema/CreateJobAPISchema.json"))
+			.body("message", equalTo("Job created successfully. "))
+			.body("data.mst_service_location_id", equalTo(1))
+			.body("data.job_number", startsWith("JOB_"));
 	}
 
 }
